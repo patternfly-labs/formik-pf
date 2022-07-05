9c6ab7987bc04a6a159ad9ea8f184a9b5201792b
Rohit Rai <rohitkrai03@gmail.com>
2022-07-05 19:53:00 +0530

    Update test config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ module.exports = {
   moduleNameMapper: {
     '.(css|less|scss)$': 'identity-obj-proxy',
   },
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/storybook-static/',
+  ],
+  roots: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!@openshift|@patternfly|@redhat-cloud-services|lodash-es|@popperjs|i18next)',
+  ],
 };
